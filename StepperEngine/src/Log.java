04aecbc7f50677e27c6a5a4e6e28e8c746c03ecf
igalmk <igal678@gmail.com>
2023-04-05 10:34:00 +0300

    worked on Log

--- StepperEngine/src/Log.java
+++ StepperEngine/src/Log.java
@@ -1,2 +1,12 @@
+import java.util.Date;
+
 public class Log {
+    String LogDesctiption;
+    Date LogTimeStamp;
+
+    public void Log(String logDesctiption)
+    {
+        this.LogDesctiption=logDesctiption;
+        LogTimeStamp=new Date();
+    }
 }
