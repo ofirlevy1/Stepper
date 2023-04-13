240b849d5922a20bb197a88708cf83867782ddd4
igalmk <igal678@gmail.com>
2023-04-13 18:40:19 +0300

    added PropertiesExporterStep

--- StepperEngine/src/Main.java
+++ StepperEngine/src/Main.java
@@ -20,20 +20,31 @@ public class Main {
         arr.add(new FileType(new File("D:\\tasks\\java\\Stepper\\StepperEngine\\src\\text2.txt")));
         arr.add(new FileType(new File("D:\\tasks\\java\\Stepper\\StepperEngine\\src\\text3.txt")));
         
-        ListType arr2 = new ListType(arr);//ListType recieves a list of StepDataTypes
+        ListType arr2 = new ListType(arr);//ListType receives a list of StepDataTypes
         ArrayList<DataType> arr3=new ArrayList<>();
         arr3.add(arr2);
         arr3.add(new NumberType(9));
 
+        Relation relation=new Relation(0,0/*,"name","age","eye color"*/);
+        //relation.set(0,0,"asher");
+        //relation.set(0,1,"25");
+        //relation.set(0,2,"green");
+        //relation.set(1,0,"neta");
+        //relation.set(1,1,"39");
+        //relation.set(1,2,"brown");
+        ArrayList<DataType> arr4=new ArrayList<>();
+        arr4.add(new RelationType(relation));
+
         // testing the presentable strings
-        for(DataType step : arr3)
+        for(DataType step : arr4)
             System.out.println(step.getPresentableString());
             
-        Step fds=new FilesContentExtractorStep(arr3,null);
+        Step fds=new PropertiesExporterStep(arr4,null);
         fds.execute();
         
         System.out.println(fds.getLogsAsString());
         System.out.println(fds.getSummaryLine());
+        System.out.println(fds.getOutputs().get(0).getPresentableString());
 
     }
 }
---
+++ StepperEngine/src/Steps/PropertiesExporterStep.java
@@ -0,0 +1,55 @@
+package Steps;
+
+import DataTypes.DataType;
+import DataTypes.Relation;
+import DataTypes.RelationType;
+import DataTypes.StringType;
+
+import java.util.ArrayList;
+
+public class PropertiesExporterStep extends Step{
+
+    public class EmptyPropertiesRelationException extends Exception{
+        public EmptyPropertiesRelationException(String str){
+            super(str);
+        }
+    }
+
+    public PropertiesExporterStep(ArrayList<DataType> inputs, ArrayList<DataType> outputs) {
+        super("PROPERTIES_EXPORTER", true, inputs, outputs);
+    }
+
+    @Override
+    public void execute() {
+        try{
+            this.runStepFlow();
+        } catch (EmptyPropertiesRelationException e) {
+            this.outputs.add(new StringType(""));
+            this.setStatus(Status.Warning);
+            this.setSummaryLine(e.getMessage());
+            this.addLog(e.getMessage());
+        } catch (Exception e) {
+            this.setSummaryLine(e.getMessage());
+            this.addLog(e.getMessage());
+            this.setStatus(Status.Failure);
+        }
+    }
+
+    @Override
+    protected void runStepFlow() throws Exception {
+        Relation relation=(Relation) this.inputs.get(0).getData();
+        String properties="";
+        this.addLog("About to process "+relation.getRows()+" lines of data");
+        if(relation.getRows()==0&&relation.getCols()==0) throw new EmptyPropertiesRelationException("Properties Relation is empty");
+        for(int i=0;i<relation.getRows();i++){
+            for(int j=0;j< relation.getCols();j++){
+                properties+="row-"+(i+1)+"."+relation.getColumnNames()[j]+"="+relation.get(i,j)+"\n";
+            }
+        }
+
+        this.setStatus(Status.Success);
+        this.addLog("Extracted total of "+(relation.getRows()* relation.getCols())+" properties");
+        this.setSummaryLine("Extracted total of "+(relation.getRows()* relation.getCols())+" properties");
+        this.outputs.add(new StringType(properties.substring(0,properties.length()-1)));//getting rid of unnecessary new line at end of string
+    }
+}
