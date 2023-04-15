7ffac341c4c07e7f1e9a43639cf47c227d6b75ba
igalmk <igal678@gmail.com>
2023-04-15 19:34:18 +0300

    fineshed FileDumperStep + tests

--- StepperEngine/src/Steps/FileDumperStep.java
+++ StepperEngine/src/Steps/FileDumperStep.java
@@ -2,19 +2,49 @@ package Steps;
 
 import DataTypes.StringType;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileDumperStep extends  Step{
 
     private StringType content;
     private StringType fileName;
 
-    public FileDumperStep(StringType content, StringType fileName){super("FILE_DUMPER", true);}
+    public FileDumperStep(StringType content, StringType fileName){
+        super("FILE_DUMPER", true);
+        this.content=content;
+        this.fileName=fileName;
+    }
     @Override
     public void execute() {
+        try {
+            runStepFlow();
+        } catch (Exception e) {
+            SetStatusAndLog(Status.Failure, e.getMessage(), e.getMessage());
+            this.outputs.add(new StringType("Failure"));
+        }
 
     }
 
     @Override
     protected void runStepFlow() throws Exception {
+        String content=(String) this.content.getData();
+        String fileName=(String) this.fileName.getData();
+        addLog("About to create file named "+ fileName);
+        File file=new File(fileName);
+        if(file.exists()) throw new Exception("File already exists");
+        try(BufferedWriter out=new BufferedWriter(new OutputStreamWriter((new FileOutputStream(fileName))))) {
+            if(content.length()==0)
+                SetStatusAndLog(Status.Warning, "Created file with no content", "entered an empty string");
+            else
+            {
+                setStatus(Status.Success);
+                setSummaryLine("Created file successfully");
+            }
+            out.write(content);
+        }
 
+        this.outputs.add(new StringType("Success"));
     }
 }
