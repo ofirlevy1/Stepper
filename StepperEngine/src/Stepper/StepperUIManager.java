d3dc17dfa5f8c8c97cc316a8ab1bd9988e483f3d
Ofir Levy <ofirly@mta.ac.il>
2023-04-22 01:49:03 +0300

    Add getFlowNames to StepperUIManager

--- StepperEngine/src/Stepper/StepperUIManager.java
+++ StepperEngine/src/Stepper/StepperUIManager.java
@@ -3,11 +3,16 @@ package Stepper;
 // This object acts as a Facade, offering the Stepper system UI functions
 // and managing the stepper object itself
 
+import Flow.Flow;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class StepperUIManager {
     Stepper stepper;
     boolean isLoaded;
+    HashSet<Flow> flows;
 
     public StepperUIManager() {
         isLoaded = false;
@@ -15,5 +20,13 @@ public class StepperUIManager {
 
     public void LoadStepperFromXmlFile(File xmlFile) {
 
+        // don't forget to set isLoaded
+    }
+
+    public ArrayList<String> getFlowNames(){
+        ArrayList<String> flowNames = new ArrayList<>();
+        for(Flow flow : flows)
+            flowNames.add(flow.getName());
+        return flowNames;
     }
 }
