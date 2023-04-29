2facf8966a2d951e3566d3dfc1a23975e6296ebb
igalmk <igal678@gmail.com>
2023-04-29 17:57:46 +0300

    fixed getPresentableString

--- StepperEngine/src/DataTypes/MappingType.java
+++ StepperEngine/src/DataTypes/MappingType.java
@@ -10,6 +10,6 @@ public class MappingType extends DataType<Mapping>{
     public MappingType(Mapping mapping, String name, boolean isInput){super(name, name.toLowerCase().replace('_',' '), mapping, Type.MAPPING, isInput);}
     @Override
     public String getPresentableString() {
-        return "car: "+getData().getCar()+"\ncdr: "+getData().getCdr();
+        return "car: "+getData().getCar().getData()+"\ncdr: "+getData().getCdr().getData();
     }
 }
