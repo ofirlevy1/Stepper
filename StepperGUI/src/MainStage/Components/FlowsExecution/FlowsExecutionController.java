31bb0e8da95cdade38173ab9ba426b5dae7eed4e
igalmk <igal678@gmail.com>
2023-05-29 16:55:45 +0300

    starting 2nd screen

---
+++ StepperGUI/src/MainStage/Components/FlowsExecution/FlowsExecutionController.java
@@ -0,0 +1,38 @@
+package MainStage.Components.FlowsExecution;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+
+public class FlowsExecutionController {
+
+    @FXML
+    private FlowPane flowInputsFlowPane;
+
+    @FXML
+    private FlowPane flowDetailsFlowPane;
+
+    @FXML
+    private FlowPane executionDetailsFlowPane;
+
+    @FXML
+    private Button flowContinuationExecuteButton;
+
+    @FXML
+    private FlowPane continuationDataFlowPane;
+
+    @FXML
+    private Button startFlowExecutionButton;
+
+    @FXML
+    void flowContinuationExecuteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void startFlowExecutionAction(ActionEvent event) {
+
+    }
+
+}
