a5e2217caf0d1dde8c0b77b73c349c8df3b3adfa
igalmk <igal678@gmail.com>
2023-06-26 15:54:14 +0300

    added new module for admin client

---
+++ StepperAdminClient/src/MainStage/Components/Main/MainStepperAdminClientController.java
@@ -0,0 +1,24 @@
+package MainStage.Components.Main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+
+public class MainStepperAdminClientController {
+
+    @FXML
+    private Label adminNameLabel;
+
+    @FXML
+    private Label selectedFileLabel;
+
+    @FXML
+    private TabPane selectionTabPane;
+
+    @FXML
+    void loadFileButtonAction(ActionEvent event) {
+
+    }
+
+}
---
+++ StepperAdminClient/src/MainStage/MainStage.java
@@ -0,0 +1,34 @@
+package MainStage;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class MainStage extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader=new FXMLLoader();
+
+        URL mainFXML=getClass().getResource("/MainStage/Components/Main/MainStepper.fxml");
+        loader.setLocation(mainFXML);
+        ScrollPane root=loader.load();
+
+//        MainStepperController mainStepperController=loader.getController();
+//        mainStepperController.setPrimaryStage(primaryStage);
+
+        primaryStage.setTitle("Stepper");
+        Scene scene=new Scene(root,1510, 950);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public void run(String[] args){
+        launch(args);
+    }
+}
