dfe33a3ce0316fd44326913182a2bfb70f7db3f1
igalmk <igal678@gmail.com>
2023-06-29 13:54:58 +0300

    added roles management tab

---
+++ StepperAdminClient/src/MainStage/Components/RolesManagement/UsersManagementController.java
@@ -0,0 +1,23 @@
+package MainStage.Components.RolesManagement;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.FlowPane;
+
+public class UsersManagementController {
+
+    @FXML
+    private FlowPane usersFlowPane;
+
+    @FXML
+    private FlowPane selectedUserFlowPane;
+
+    @FXML
+    private FlowPane rolesAssignmentToUser;
+
+    @FXML
+    void saveButtonAction(ActionEvent event) {
+
+    }
+
+}
