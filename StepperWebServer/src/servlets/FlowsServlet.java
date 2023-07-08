79235b3569fcaaf25b7fc70b55665cb6ef6994df
Ofir Levy <ofirly@mta.ac.il>
2023-07-08 23:50:39 +0300

    Add servlet for getting all flows names

---
+++ StepperWebServer/src/servlets/FlowsServlet.java
@@ -0,0 +1,42 @@
+package servlets;
+
+
+import Stepper.StepperUIManager;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import utils.ServletUtils;
+import utils.SessionUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet(name = "Flows Servlet", urlPatterns = "/flows")
+public class FlowsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = SessionUtils.getUsername(req);
+        StepperUIManager stepperUIManager = ServletUtils.getStepperUIManager(getServletContext());
+        ArrayList<String> flowsNames = null;
+
+        if (username == null) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().println("The client is not logged in! please login first (/login)");
+            return;
+        }
+
+        try {
+            flowsNames = stepperUIManager.getFlowNames();
+            Gson gson = new Gson();
+            resp.getWriter().println(gson.toJson(flowsNames));
+        }
+        catch(Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().println(e.getMessage());
+        }
+
+    }
+}
