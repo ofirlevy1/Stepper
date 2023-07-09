fa82a3ee4bb543d5970e0bb057c95c79e21002b3
Ofir Levy <ofirly@mta.ac.il>
2023-07-10 01:06:11 +0300

    Add FlowDescriptor servlet

---
+++ StepperWebServer/src/servlets/FlowDescriptorServlet.java
@@ -0,0 +1,51 @@
+package servlets;
+
+
+import Flow.FlowDescriptor;
+import Stepper.StepperUIManager;
+import Users.UserDescriptor;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import utils.ServletUtils;
+import utils.SessionUtils;
+
+import java.io.IOException;
+
+@WebServlet(name = "User Servlet", urlPatterns = "/flow")
+public class FlowDescriptorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = SessionUtils.getUsername(req);
+        StepperUIManager stepperUIManager = ServletUtils.getStepperUIManager(getServletContext());
+        FlowDescriptor flowDescriptor = null;
+
+        String flowName = req.getParameter("flow_name");
+
+        if (username == null) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().println("The client is not logged in! please login first (/login)");
+            return;
+        }
+
+        if (flowName == null) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().println("Required 'flow_name' query parameter was not found in the request.");
+            return;
+        }
+
+        try {
+            flowDescriptor = stepperUIManager.getFlowDescriptor(flowName);
+        }
+        catch(Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().println(e.getMessage());
+        }
+
+        Gson gson = new Gson();
+        resp.getWriter().println(gson.toJson(flowDescriptor));
+    }
+}
